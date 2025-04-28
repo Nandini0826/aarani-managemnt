4c8764cf75b9f4c26115f9119a21d070146386e3
priyanshu.b0902@gmail.com <priyanshu.b0902@gmail.com>
2025-04-28 18:38:56 +0530

    CheckPoint 1

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const db = require("./config/mongoose-connection");
+const path = require('path');
+const PORT = process.env.PORT;
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+   expressSession({
+       resave: false,  // resave: for saving everytime, false means to not save everytime if there is no change
+       saveUninitialized: false, // to not create a session for a user who is not logged in or not initialized
+       secret: process.env.EXPRESS_SESSION_SECRET,  // secret is handled using environment variables
+   })
+);
+app.use(flash());
+
+app.listen(PORT, (err)=>{
+   if(err) return console.log(err);
+   else console.log("Server is running at PORT:", PORT);
+});
