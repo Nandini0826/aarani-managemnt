c6ff342a19c84c37fd7265804813f9155b2442f5
priyanshu.b0902@gmail.com <priyanshu.b0902@gmail.com>
2025-04-29 23:52:25 +0530

    checkpoint 5

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const db = require("./config/mongoose-connection");
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT;
 const expressSession = require("express-session");
 const flash = require("connect-flash");
-const indexRouter = require('./routes/index-router');
-const productRouter = require('./routes/product-router')
+const MongoStore = require("connect-mongo");
+const indexRouter = require("./routes/index-router");
+const productRouter = require("./routes/product-router");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
-   expressSession({
-       resave: false,  // resave: for saving everytime, false means to not save everytime if there is no change
-       saveUninitialized: false, // to not create a session for a user who is not logged in or not initialized
-       secret: process.env.EXPRESS_SESSION_SECRET,  // secret is handled using environment variables
-   })
+  expressSession({
+    resave: false, // resave: for saving everytime, false means to not save everytime if there is no change
+    saveUninitialized: false, // to not create a session for a user who is not logged in or not initialized
+    secret: process.env.EXPRESS_SESSION_SECRET, // secret is handled using environment variables
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      ttl: 14 * 24 * 60 * 60, // Optional: session expiry in seconds
+    }),
+  })
 );
 app.use(flash());
 
-app.use('/', indexRouter);
-app.use('/product', productRouter);
+app.use("/", indexRouter);
+app.use("/product", productRouter);
 
-app.listen(PORT, (err)=>{
-   if(err) return console.log(err);
-   else console.log("Server is running at PORT:", PORT);
+app.listen(PORT, (err) => {
+  if (err) return console.log(err);
+  else console.log("Server is running at PORT:", PORT);
 });
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,27 +1,28 @@
 const mongoose = require("mongoose");
-const debug = require('debug');
-const config = require('config');
+const debug = require("debug");
+const config = require("config");
 
-const env = process.env.NODE_ENV || "development"
+const env = process.env.NODE_ENV || "development";
 const dbgr = debug(`${env}:mongoose`);
 
-if(env=="development") {
-   mongoose.connect(`${config.get("MONGO_URI")}/aarani`)
-   .then(function(){
+if (env == "development") {
+  mongoose
+    .connect(`${config.get("MONGO_URI")}/aarani`)
+    .then(function () {
       dbgr("connected");
-   })
-   .catch(function(err){
+    })
+    .catch(function (err) {
       dbgr(err);
-   });
-}
-else if(env=="production") {
-   mongoose.connect(`${process.env.MONGODB_URI}/aarani`)
-   .then(function(){
+    });
+} else if (env == "production") {
+  mongoose
+    .connect(`${process.env.MONGODB_URI}`)
+    .then(function () {
       dbgr("connected");
-   })
-   .catch(function(err){
+    })
+    .catch(function (err) {
       dbgr(err);
-   });
+    });
 }
 
 module.exports = mongoose.connection;
